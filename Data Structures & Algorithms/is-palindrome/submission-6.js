5a4606222281cd84d790f40456b072aec347016f
Vikash Chandra Maurya <mauryavikash922@gmail.com>
2026-07-27 09:19:32 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+    isAlphanumeric(char){
+
+        const code=char.charCodeAt(0);
+        return (code >= 65 && code<=90) || (code >=48 && code <=57) || (code >=97 && code<=122 ) 
+    }
+    isPalindrome(s) {
+        let i=0;
+        let j=s.length-1;
+
+
+        while(i<j && i<s.length && j>=0){
+            if(!this.isAlphanumeric(s[i])){
+                i++;
+                continue
+            }
+            if(!this.isAlphanumeric(s[j])){
+                j--;
+                continue
+            }
+
+            if(i>=j || j<0 || i>=s.length){
+                break;
+            }
+            if(s[i].toLowerCase()!=s[j].toLowerCase()){
+                return false;
+            }else{
+                i++;
+                j--;
+            }
+            
+        }
+
+        return true;
+    }
+}
