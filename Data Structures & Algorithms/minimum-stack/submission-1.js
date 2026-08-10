08668b868651779cc06f6d5a887f22223c876fcc
Vikash Chandra Maurya <mauryavikash922@gmail.com>
2026-08-10 21:58:05 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.js
@@ -0,0 +1,74 @@
+class MinStack {
+    /**
+     * 
+     * 2,4,1,0,1,5
+     * 
+     * 4 2 1 1 0 
+     */
+    constructor() {
+        this.items=[];
+        this.minStack=[];
+
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+
+        this.items.push(val);
+        const minVal=Math.min(
+            val, this.minStack.length==0?val:this.minStack[this.minStack.length-1]
+        )
+        this.minStack.push(minVal);
+
+        // this.items.push(val);
+        // while(this.minStack[this.minStack.length-1] < val){
+        //     this.tempStack.push(this.minStack.pop());
+        // }
+        // this.minStack.push(val);
+        // while(this.tempStack.length){
+        //     this.minStack.push(this.tempStack.pop());
+        // }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        if(this.items.length==0){
+            return null
+        }
+        this.minStack.pop();
+        return this.items.pop();
+
+        // if(this.items.length==0){
+        //     return null;
+        // }
+        // const val=this.items.pop();
+        // while(this.minStack[this.minStack.length-1]!=val){
+        //     this.tempStack.push(this.minStack.pop());
+        // }
+        // this.minStack.pop();
+        // while(this.tempStack.length!==0){
+        //     this.minStack.push(this.tempStack.pop())
+        // }
+
+        // return val;
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.items.length? this.items[this.items.length-1] :null
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.items.length? this.minStack[this.minStack.length-1]:null
+    }
+}
