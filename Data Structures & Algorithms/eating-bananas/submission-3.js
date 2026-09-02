1f99dae0f17f7da54ca62c3620063cede2629c72
Vikash Chandra Maurya <mauryavikash922@gmail.com>
2026-09-02 21:22:16 +0530

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    getTime(arr, k) {
+        return arr.reduce((acc, val) => {
+            return acc + Math.ceil(val / k)
+        }, 0);
+    }
+    minEatingSpeed(piles, h) {
+        const m = Math.max(...piles);
+
+        let l = 1, r = m;
+        let min = m;
+
+        while (l <= r) {
+            const mid = Math.floor((l + r) / 2);
+            const time = this.getTime(piles, mid);
+            console.log("time ",time,mid)
+            if (time > h) {
+                l = mid + 1;
+            } else {
+                min = mid
+                r = mid - 1
+            }
+        }
+
+        return min;
+    }
+}
